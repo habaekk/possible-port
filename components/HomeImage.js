bb70e3b64e9b755b639abfafea1fd6edf9ff5e01
habaekk <haback1493@gmail.com>
2024-06-27 20:52:38 +0900

    add quotation box to homeImage

--- components/HomeImage.js
+++ components/HomeImage.js
@@ -10,6 +10,13 @@ const HomeImage = () => (
       layout="fill" // 이미지가 컨테이너를 꽉 채우도록 설정
       objectFit="cover" // 이미지 비율 유지하며 꽉 채우기
     />
+    <div className="quote-box">
+      <div className="quote">
+        <span className="quote-mark left-quote">“</span>
+        <p className="quote-text">Home of risk takers, late brakers and history makers 💫</p>
+        <span className="quote-mark right-quote">”</span>
+      </div>
+    </div>
   </div>
 );
 
