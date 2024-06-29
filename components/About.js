c9fca02acf5948699520235979573fea80c2916d
habaekk <haback1493@gmail.com>
2024-06-29 15:45:03 +0900

    change language to Korean in About me

--- components/About.js
+++ components/About.js
@@ -2,19 +2,21 @@ import React from 'react';
 
 const About = () => (
   <section id="about" className="p-8">
-    <h2 className="text-2xl font-bold mb-4">About Me</h2>
-    <p>Bachelor of Computer Science Student at SeoulTech</p>
-    <p>Email: haback1493@gmail.com</p>
-    <p>Github: <a href="https://github.com/habaekk" target="_blank" rel="noopener noreferrer">https://github.com/habaekk</a></p>
+    <h2 className="text-2xl font-bold mb-4">내 정보</h2>
+    <p>한 줄 소개</p>
+    <p>이메일: haback1493@gmail.com</p>
+    <p>깃허브: <a href="https://github.com/habaekk" target="_blank" rel="noopener noreferrer">https://github.com/habaekk</a></p>
 
-    <h2 className="text-2xl font-bold mt-8 mb-4">Education</h2>
-    <p><strong>Seoul National University of Science and Technology</strong></p>
-    <p>Bachelor of Computer Science GPA: 3.93 / 4.5</p>
+    <h2 className="text-2xl font-bold mt-8 mb-4">교육</h2>
+    <p><strong>서울과학기술대학교</strong></p>
+    <p>컴퓨터공학과 학사</p>
+    <p>학점평균: 3.93 / 4.5</p>
 
-    <h2 className="text-2xl font-bold mt-8 mb-4">Certifications & Qualifications</h2>
+    <h2 className="text-2xl font-bold mt-8 mb-4">자격증</h2>
     <ul className="list-disc list-inside">
       <li>TOEIC - 930/990</li>
       <li>TOEIC Speaking - 170 (AL)</li>
+      <li>정보처리기사</li>
       <li>SQLD</li>
     </ul>
   </section>
--- components/HomeImage.js
+++ components/HomeImage.js
@@ -13,7 +13,7 @@ const HomeImage = () => (
     <div className="quote-box">
       <div className="quote">
         <span className="quote-mark left-quote">“</span>
-        <p className="quote-text">액션, 드라마, 언더독, 스토리</p>
+        <p className="quote-text">ㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁ</p>
         <span className="quote-mark right-quote">”</span>
       </div>
     </div>
