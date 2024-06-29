df025d0e4a61041be7321d36e15d644a2e46ab53
habaekk <haback1493@gmail.com>
2024-06-29 21:01:14 +0900

    edit about me

--- components/About.js
+++ components/About.js
@@ -1,25 +1,42 @@
 import React from 'react';
+import Image from 'next/image';
 
 const About = () => (
   <section id="about" className="p-8">
     <h2 className="text-2xl font-bold mb-4">내 정보</h2>
-    <p className="font-bold">ㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁ</p>
-    <p className="font-bold">ㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁ</p>
-    <p>이메일: haback1493@gmail.com</p>
-    <p>깃허브: <a href="https://github.com/habaekk" target="_blank" rel="noopener noreferrer">https://github.com/habaekk</a></p>
-
-    <h2 className="text-2xl font-bold mt-8 mb-4">교육</h2>
-    <p><strong>서울과학기술대학교</strong></p>
-    <p>컴퓨터공학과 학사</p>
-    <p>학점평균: 3.93 / 4.5</p>
-
-    <h2 className="text-2xl font-bold mt-8 mb-4">자격증</h2>
-    <ul className="list-disc list-inside">
-      <li>TOEIC - 930/990</li>
-      <li>TOEIC Speaking - 170 (AL)</li>
-      <li>정보처리기사</li>
-      <li>SQLD</li>
-    </ul>
+    <div className="flex mb-8">
+      <div className="w-1/3">
+        <Image
+          src="/images/me5.jpg" // 이미지 경로 설정
+          alt="Profile Image"
+          width={300} // 이미지 너비 설정
+          height={500} // 이미지 높이 설정
+          className="object-cover rounded-lg"
+        />
+      </div>
+      <div className="w-2/3 pl-8">
+        <h3 className="text-xl font-bold mb-1">박하백</h3>
+        <p className="font-bold">ㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁ</p>
+        <p className="font-bold">ㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁ</p>
+        <br />
+        <h3 className="text-xl font-bold mb-1">교육</h3>
+        <p><strong>서울과학기술대학교 (학사)</strong></p>
+        <p>ITM --{'>'} 컴퓨터공학과 [ 3.93 / 4.5 ]</p>
+        <br />
+        <p><strong>Udemy (Online Education)</strong></p>
+        <p>iOS & Swift - The Complete iOS App Development Bootcamp
+        </p>
+        <br />
+        <h3 className="text-xl font-bold mb-1">자격증</h3>
+        <ul className="list-disc list-inside">
+          <li>TOEIC - 930/990</li>
+          <li>TOEIC Speaking - 170 (AL)</li>
+          <li>정보처리기사</li>
+          <li>SQLD</li>
+        </ul>
+      </div>
+    </div>    
+    
   </section>
 );
 
--- components/HomeImage.js
+++ components/HomeImage.js
@@ -5,7 +5,7 @@ import './HomeImage.css';
 const HomeImage = () => (
   <div className="home-image-container">
     <Image
-      src="/images/me3.jpg" // 이미지 경로 설정
+      src="/images/sample.jpg" // 이미지 경로 설정
       alt="Home Image"
       layout="fill" // 이미지가 컨테이너를 꽉 채우도록 설정
       objectFit="cover" // 이미지 비율 유지하며 꽉 채우기
