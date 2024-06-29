d59918ee2ac71c088194eec665d2335756059de2
habaekk <haback1493@gmail.com>
2024-06-30 00:02:50 +0900

    solve params error. should work further for cleaning code and logic

--- app/[projectName]/page.tsx
+++ app/[projectName]/page.tsx
@@ -3,16 +3,12 @@
 import { useParams } from 'next/navigation';
 import projects from '../data/projects';
 
-const ProjectDetailPage = () => {
-  const { projectId } = useParams();
-  const project = projects.find((project) => project.id === parseInt(projectId, 10));
-
-  if (!project) return <p>프로젝트를 찾을 수 없습니다.</p>;
+const ProjectDetailPage = (props: any) => {
 
   return (
     <div className="p-8">
-      <h1 className="text-3xl font-bold mb-4">{project.title}</h1>
-      <p>{project.description}</p>
+      <h1 className="text-3xl font-bold mb-4">{props.params.projectName}</h1>
+      {/* <p>{project.description}</p> */}
       {/* 추가적인 프로젝트 상세 정보 표시 */}
     </div>
   );
