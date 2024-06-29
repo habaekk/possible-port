ffe56ead5cf09dd1f9ffc6bbf98c0d627f96656e
habaekk <haback1493@gmail.com>
2024-06-30 00:05:59 +0900

    show project data from data folder. issue of fix layout and header later

--- app/[projectName]/page.tsx
+++ app/[projectName]/page.tsx
@@ -4,11 +4,13 @@ import { useParams } from 'next/navigation';
 import projects from '../data/projects';
 
 const ProjectDetailPage = (props: any) => {
+    const project = projects.find((project) => project.id === parseInt(props.params.projectName, 10));
 
+    if (!project) return <p>프로젝트를 찾을 수 없습니다.</p>;
   return (
     <div className="p-8">
-      <h1 className="text-3xl font-bold mb-4">{props.params.projectName}</h1>
-      {/* <p>{project.description}</p> */}
+      <h1 className="text-3xl font-bold mb-4">{project.title}</h1>
+      <p>{project.description}</p>
       {/* 추가적인 프로젝트 상세 정보 표시 */}
     </div>
   );
