35e7e79f9011a4fc3abc7cc4e94b923bc3c70b7f
habaekk <haback1493@gmail.com>
2024-07-02 21:13:31 +0900

    unify to ProjectDetails.js

--- app/[projectName]/page.tsx
+++ app/[projectName]/page.tsx
@@ -1,10 +1,7 @@
 "use client";
 
 import projects from '../data/projects';
-import MoonShapeWidget from '../../components/projects/MoonShapeWidget';
-import WateringWidget from '../../components/projects/WateringWidget';
-import DefaultProject from '../../components/projects/DefaultProject';
-
+import ProjectDetail from '../../components/projects/ProjectDetail';
 const ProjectDetailPage = (props) => {
   const projectTitle = props.params.projectName;
   const project = projects.find(
@@ -13,14 +10,7 @@ const ProjectDetailPage = (props) => {
 
   if (!project) return <p>프로젝트를 찾을 수 없습니다.</p>;
 
-  switch (project.title) {
-    case 'Moon Shape Widget':
-      return <MoonShapeWidget project={project} />;
-    case 'Watering Widget':
-      return <WateringWidget project={project} />;
-    default:
-      return <DefaultProject project={project} />;
-  }
+  return <ProjectDetail project={project} />;
 };
 
 export default ProjectDetailPage;
--- app/data/projects.tsx
+++ app/data/projects.tsx
@@ -1,11 +1,40 @@
 const projects = [
-    { id: 1, title: 'Moon Shape Widget', description: 'IOS Application' },
-    { id: 2, title: 'Watering Widget', description: 'Android Application' },
-    { id: 3, title: 'CatSAMA', description: 'React-Native Application' },
-    { id: 4, title: 'Impossible Possibility Portfolio', description: 'Next.JS Web Application' },
-    { id: 5, title: 'SwingBy-RL', description: 'Unity Reinforcement Learning Program' },
-    { id: 6, title: 'Story and Draw', description: 'RenPY Visual Novel Game' },
-  ];
-  
-  export default projects;
-  
+  {
+    id: 1,
+    title: 'Moon Shape Widget',
+    description: 'IOS Application',
+    motivation: 'Moon Shape Widget 개발 계기...',
+    features: 'Moon Shape Widget 기능 설명...',
+    problemSolving: 'Moon Shape Widget 문제 해결...',
+    achievements: 'Moon Shape Widget 성과 및 배운 점...',
+  },
+  {
+    id: 2,
+    title: 'Watering Widget',
+    description: 'Android Application',
+    motivation: 'Watering Widget 개발 계기...',
+    features: 'Watering Widget 기능 설명...',
+    problemSolving: 'Watering Widget 문제 해결...',
+    achievements: 'Watering Widget 성과 및 배운 점...',
+  },
+  {
+    id: 3,
+    title: 'CatSAMA',
+    description: 'React-Native Application',
+    motivation: 'CatSAMA 개발 계기...',
+    features: 'CatSAMA 기능 설명...',
+    problemSolving: 'CatSAMA 문제 해결...',
+    achievements: 'CatSAMA 성과 및 배운 점...',
+  },
+  {
+    id: 4,
+    title: 'Impossible Possibility Portfolio',
+    description: 'Next.JS Web Application',
+    motivation: 'Impossible Possibility Portfolio 개발 계기...',
+    features: 'Impossible Possibility Portfolio 기능 설명...',
+    problemSolving: 'Impossible Possibility Portfolio 문제 해결...',
+    achievements: 'Impossible Possibility Portfolio 성과 및 배운 점...',
+  },
+];
+
+export default projects;
