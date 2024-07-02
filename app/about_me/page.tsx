6e9eb3b6764f6d122de7c535427043d28ac60025
habaekk <haback1493@gmail.com>
2024-07-02 23:52:08 +0900

    edit about me

--- app/about_me/page.tsx
+++ app/about_me/page.tsx
@@ -2,21 +2,25 @@ import React from 'react';
 
 const AboutMePage = () => (
   <div className="p-8">
-    <h2 className="text-3xl font-bold mb-4">About Me</h2>
+    <h2 className="text-3xl font-bold mb-4">나에 대해</h2>
     <section className="mb-8">
-        <p>여기에 본인의 소개글을 작성하세요.</p>
+        <p>한 줄 소개</p>
     </section>
     <section className="mb-8">
-      <h3 className="text-2xl font-bold mb-4">교육</h3>
-      <p>여기에 교육 내용을 작성하세요.</p>
+      <h3 className="text-2xl font-bold mb-4">왜 개발자는 직업을 하고 싶은지?</h3>
+      <p>여기에 계기 내용을 작성하세요.</p>
     </section>
     <section className="mb-8">
-      <h3 className="text-2xl font-bold mb-4">경력</h3>
-      <p>여기에 경력 내용을 작성하세요.</p>
+      <h3 className="text-2xl font-bold mb-4">개발 전반에서 가장 관심있는것</h3>
+      <p>여기에 관심 내용을 작성하세요.</p>
     </section>
     <section className="mb-8">
-      <h3 className="text-2xl font-bold mb-4">기타</h3>
-      <p>여기에 기타 내용을 작성하세요.</p>
+      <h3 className="text-2xl font-bold mb-4">기술 스택과 개발 도구, 어떤걸로 뭘 해봤는지 ?</h3>
+      <p>여기에 기술 내용을 작성하세요.</p>
+    </section>
+    <section className="mb-8">
+      <h3 className="text-2xl font-bold mb-4">어떤 개발자가 되고 싶고, 어떤 걸 하고싶은지?</h3>
+      <p>여기에 미래 내용을 작성하세요.</p>
     </section>
   </div>
 );
