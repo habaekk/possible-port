3a8341051c2ea076405812e34f972275a7f58fcf
habaekk <haback1493@gmail.com>
2024-07-05 21:08:43 +0900

    edit content of Watering Widget

--- components/projects/WateringWidget.js
+++ components/projects/WateringWidget.js
@@ -17,19 +17,18 @@ const WateringWidget = ({ project }) => (
       <h2 className="text-2xl font-bold mb-4">문제 해결</h2>
       <p className="mb-2">1. <strong>개발 동기</strong></p>
       <p className="mb-4">친구의 요청으로 만들어진 어플입니다.</p>
-      <p className="mb-2">2. <strong>위젯 로직</strong> </p>
-      <p className="mb-4"></p>
-      <p className="mb-2">3. <strong>위젯과 어플간의 데이터 일관성</strong></p>
+      <p className="mb-2">2. <strong>위젯과 어플간의 데이터 일관성</strong></p>
       <p className="mb-4">위젯을 통해 기록한 물 섭취량이 어플에 반영되어야 함.</p>
+      <p className="mb-2">3. <strong>개발자 계정 & 파일 소실</strong></p>
+      <p className="mb-4">장기 비활동으로 인한 계정 정지, 맥북 이동으로 인한 데이터 소실</p>
 
     </section>
 
     <section className="mb-8">
       <h2 className="text-2xl font-bold mb-4">배운점</h2>
-      <p>IOS 와 안드로이드 개발의 차이</p>
+      <p className="mb-4">1. <strong>안드로이드 개발의 특징, IOS 개발과 다른점</strong></p>
+      <p className="mb-4">2. <strong>계정 및 파일 관리의 중요성</strong></p>
     </section>
-
-    {/* Watering Widget에 대한 추가 내용 */}
   </div>
 );
 
